15bb1c4753021db9bc5265a03335c2bef5f2fc06
pryority <matthewapryor@gmail.com>
2023-11-28 19:30:19 -0500

    feat(*): initial commit

---
+++ src/MockERC20.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import {ERC20} from "@solmate/tokens/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+    uint8 public constant DECIMALS = 18;
+
+    constructor() ERC20("Mock Token", "MCT", DECIMALS) {}
+
+    function mint(address _to, uint256 _amount) external {
+        _mint(_to, _amount);
+    }
+}
---
+++ test/MockERC20.t.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.17;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {MockERC20} from "../src/MockERC20.sol";
+import {SigUtils} from "../src/SigUtils.sol";
+
+contract DepositTest is Test {
+    MockERC20 internal token;
+    SigUtils internal sigUtils;
+
+    uint256 internal ownerPrivateKey;
+    uint256 internal spenderPrivateKey;
+
+    address internal owner;
+    address internal spender;
+
+    function setUp() public {
+        // Deploy a mock ERC-20 token and SigUtils helper with the token's EIP-712 domain separator
+        token = new MockERC20();
+        sigUtils = new SigUtils(token.DOMAIN_SEPARATOR);
+
+        // Create private keys to mock the owner and spender
+        ownerPrivateKey = 0xA11CE;
+        spenderPrivateKey = 0xB0B;
+
+        // Derive owner and spender addresses using the vm.addr cheatcode
+        owner = vm.addr(ownerPrivateKey);
+        spender = vm.addr(spenderPrivateKey);
+
+        // Mint the owner a test token
+        token.mint(owner, 1e18);
+    }
+}
